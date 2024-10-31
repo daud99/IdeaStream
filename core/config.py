591cf89d808b15a2745005137163e6e62ae06b9d
ali_ahmed98 <umar.mukhtar4455@gmail.com>
2024-10-31 15:07:19 +0500

    update mongodb config

--- core/config.py
+++ core/config.py
@@ -11,7 +11,7 @@ class Settings:
     RELOAD: bool = os.getenv("RELOAD", "true").lower() == "true"
     OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
     OPENAI_REALTIME_URL = "wss://api.openai.com/v1/realtime?model=gpt-4o-realtime-preview-2024-10-01"
-    MONGODB_URL: str = os.getenv("MONGODB_URL", "mongodb://localhost:27017")
+    MONGODB_URL: str = os.getenv("MONGODB_URL", f"mongodb+srv://umarmukhtar4455:{os.getenv('MONGODB_PASSWORD')}@cluster0.e2q1w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DB_NAME: str = os.getenv("DB_NAME", "test")
     SECRET_KEY = os.getenv("SECRET_KEY")
     ALGORITHM = "HS256"
